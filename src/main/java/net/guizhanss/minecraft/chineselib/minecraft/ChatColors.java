3159197cf1eded4866f40c619a529a25a57a21be
ybw0014 <i@guizhanss.com>
2021-10-24 08:11:34 +0800

    重构ChatColors, DyeColors

--- src/main/java/net/guizhanss/minecraft/chineselib/minecraft/ChatColors.java
+++ src/main/java/net/guizhanss/minecraft/chineselib/minecraft/ChatColors.java
@@ -2,43 +2,48 @@ package net.guizhanss.minecraft.chineselib.minecraft;
 
 import lombok.Getter;
 import net.guizhanss.minecraft.chineselib.utils.StringUtil;
+import org.bukkit.ChatColor;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 /**
  * Minecraft - 聊天颜色
  */
 public enum ChatColors {
-    AQUA("Aqua", "天蓝色"),
-    BLACK("Black", "黑色"),
-    BLUE("Blue", "蓝色"),
-    BOLD("Bold", "加粗"),
-    DARK_AQUA("Dark Aqua", "湖蓝色"),
-    DARK_BLUE("Dark Blue", "深蓝色"),
-    DARK_GRAY("Dark Gray", "深灰色"),
-    DARK_GREEN("Dark Green", "深绿色"),
-    DARK_PURPLE("Dark Purple", "紫色"),
-    DARK_RED("Dark Red", "深红色"),
-    GOLD("Gold", "金色"),
-    GRAY("Gray", "灰色"),
-    GREEN("Green", "绿色"),
-    ITALIC("Italic", "斜体"),
-    LIGHT_PURPLE("Light Purple", "粉红色"),
-    MAGIC("Magic", "随机"),
-    RED("Red", "红色"),
-    RESET("Reset", "重置"),
-    STRIKETHROUGH("Strikethrough", "删除线"),
-    UNDERLINE("Underline", "下划线"),
-    WHITE("White", "白色"),
-    YELLOW("Yellow", "黄色");
+    AQUA(ChatColor.AQUA, "Aqua", "天蓝色"),
+    BLACK(ChatColor.BLACK, "Black", "黑色"),
+    BLUE(ChatColor.BLUE, "Blue", "蓝色"),
+    BOLD(ChatColor.BOLD, "Bold", "加粗"),
+    DARK_AQUA(ChatColor.DARK_AQUA, "Dark Aqua", "湖蓝色"),
+    DARK_BLUE(ChatColor.DARK_BLUE, "Dark Blue", "深蓝色"),
+    DARK_GRAY(ChatColor.DARK_GRAY, "Dark Gray", "深灰色"),
+    DARK_GREEN(ChatColor.DARK_GREEN, "Dark Green", "深绿色"),
+    DARK_PURPLE(ChatColor.DARK_PURPLE, "Dark Purple", "紫色"),
+    DARK_RED(ChatColor.DARK_RED, "Dark Red", "深红色"),
+    GOLD(ChatColor.GOLD, "Gold", "金色"),
+    GRAY(ChatColor.GRAY, "Gray", "灰色"),
+    GREEN(ChatColor.GREEN, "Green", "绿色"),
+    ITALIC(ChatColor.ITALIC, "Italic", "斜体"),
+    LIGHT_PURPLE(ChatColor.LIGHT_PURPLE, "Light Purple", "粉红色"),
+    MAGIC(ChatColor.MAGIC, "Magic", "随机"),
+    RED(ChatColor.RED, "Red", "红色"),
+    RESET(ChatColor.RESET, "Reset", "重置"),
+    STRIKETHROUGH(ChatColor.STRIKETHROUGH, "Strikethrough", "删除线"),
+    UNDERLINE(ChatColor.UNDERLINE, "Underline", "下划线"),
+    WHITE(ChatColor.WHITE, "White", "白色"),
+    YELLOW(ChatColor.YELLOW, "Yellow", "黄色");
 
     public static final char COLOR_CHAR = '\u00a7';
 
-    private @Getter String english;
-    private @Getter String chinese;
+    private final @Getter ChatColor color;
+    private final @Getter String english;
+    private final @Getter String chinese;
 
-    ChatColors(@Nonnull String english, @Nonnull String chinese) {
+    @ParametersAreNonnullByDefault
+    ChatColors(ChatColor color, String english, String chinese) {
+        this.color = color;
         this.english = english;
         this.chinese = chinese;
     }
@@ -49,12 +54,25 @@ public enum ChatColors {
     }
 
     /**
+     * 根据聊天颜色返回对应的枚举类型
+     * @param chatColor ChatColor
+     * @return 对应的枚举类型
+     */
+    public static @Nonnull ChatColors fromChatColor(@Nonnull ChatColor chatColor) {
+        for (ChatColors color : ChatColors.values()) {
+            if (color.getColor() == chatColor) {
+                return color;
+            }
+        }
+        throw new IllegalArgumentException("无效的ChatColor");
+    }
+
+    /**
      * 根据英文返回对应的枚举类型
      * @param english 提供的英文类型
      * @return 对应的枚举类型
      */
-    @Nullable
-    public static ChatColors fromEnglish(String english) {
+    public static @Nullable ChatColors fromEnglish(@Nonnull String english) {
         String humanized = StringUtil.humanize(english);
         for (ChatColors color : ChatColors.values()) {
             if (color.getEnglish().equals(humanized)) {
--- src/main/java/net/guizhanss/minecraft/chineselib/minecraft/DyeColors.java
+++ src/main/java/net/guizhanss/minecraft/chineselib/minecraft/DyeColors.java
@@ -2,36 +2,41 @@ package net.guizhanss.minecraft.chineselib.minecraft;
 
 import lombok.Getter;
 import net.guizhanss.minecraft.chineselib.utils.StringUtil;
+import org.bukkit.DyeColor;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 /**
  * 所有染料颜色
  * @author ybw0014
  */
 public enum DyeColors {
-    BLACK("Black", "黑色"),
-    BLUE("Blue", "蓝色"),
-    BROWN("Brown", "棕色"),
-    CYAN("Cyan", "青色"),
-    GRAY("Gray", "灰色"),
-    GREEN("Green", "绿色"),
-    LIGHT_BLUE("Light Blue", "淡蓝色"),
-    LIGHT_GRAY("Light Gray", "淡灰色"),
-    LIME("Lime", "黄绿色"),
-    MAGENTA("Magenta", "品红色"),
-    ORANGE("Orange", "橙色"),
-    PINK("Pink", "粉红色"),
-    PURPLE("Purple", "紫色"),
-    RED("Red", "红色"),
-    WHITE("White", "白色"),
-    YELLOW("Yellow", "黄色");
+    BLACK(DyeColor.BLACK, "Black", "黑色"),
+    BLUE(DyeColor.BLUE, "Blue", "蓝色"),
+    BROWN(DyeColor.BROWN, "Brown", "棕色"),
+    CYAN(DyeColor.CYAN, "Cyan", "青色"),
+    GRAY(DyeColor.GRAY, "Gray", "灰色"),
+    GREEN(DyeColor.GREEN, "Green", "绿色"),
+    LIGHT_BLUE(DyeColor.LIGHT_BLUE, "Light Blue", "淡蓝色"),
+    LIGHT_GRAY(DyeColor.LIGHT_GRAY, "Light Gray", "淡灰色"),
+    LIME(DyeColor.LIME, "Lime", "黄绿色"),
+    MAGENTA(DyeColor.MAGENTA, "Magenta", "品红色"),
+    ORANGE(DyeColor.ORANGE, "Orange", "橙色"),
+    PINK(DyeColor.PINK, "Pink", "粉红色"),
+    PURPLE(DyeColor.PURPLE, "Purple", "紫色"),
+    RED(DyeColor.RED, "Red", "红色"),
+    WHITE(DyeColor.WHITE, "White", "白色"),
+    YELLOW(DyeColor.YELLOW, "Yellow", "黄色");
 
-    private @Getter String english;
-    private @Getter String chinese;
+    private final @Getter DyeColor color;
+    private final @Getter String english;
+    private final @Getter String chinese;
 
-    DyeColors(@Nonnull String english, @Nonnull String chinese) {
+    @ParametersAreNonnullByDefault
+    DyeColors(DyeColor color, String english, String chinese) {
+        this.color = color;
         this.english = english;
         this.chinese = chinese;
     }
@@ -42,12 +47,26 @@ public enum DyeColors {
     }
 
     /**
+     * 根据染料颜色返回对应的枚举类型
+     * @param dyeColor DyeColor
+     * @return 对应的枚举类型
+     */
+    public static @Nonnull DyeColors fromDyeColor(@Nonnull DyeColor dyeColor) {
+        for (DyeColors color : DyeColors.values()) {
+            if (color.getColor() == dyeColor) {
+                return color;
+            }
+        }
+        throw new IllegalArgumentException("无效的DyeColor");
+    }
+
+    /**
      * 根据英文返回对应的枚举类型
      * @param english 提供的英文类型
      * @return 对应的枚举类型
      */
-    @Nullable
-    public static DyeColors fromEnglish(String english) {
+
+    public static @Nullable DyeColors fromEnglish(@Nonnull String english) {
         String humanized = StringUtil.humanize(english);
         for (DyeColors color : DyeColors.values()) {
             if (color.getEnglish().equals(humanized)) {
