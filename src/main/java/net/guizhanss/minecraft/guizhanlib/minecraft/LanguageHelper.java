10b626b78ac5c0bee8e790677583719679e4451e
ybw0014 <i@guizhanss.com>
2021-10-31 18:38:19 +0800

    更改读入

--- src/main/java/net/guizhanss/minecraft/guizhanlib/minecraft/LanguageHelper.java
+++ src/main/java/net/guizhanss/minecraft/guizhanlib/minecraft/LanguageHelper.java
@@ -1,7 +1,8 @@
 package net.guizhanss.minecraft.guizhanlib.minecraft;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.experimental.UtilityClass;
-import net.guizhanss.minecraft.guizhanlib.utils.FileUtil;
 import org.apache.commons.lang.Validate;
 import org.bukkit.Material;
 import org.bukkit.block.Biome;
@@ -13,6 +14,10 @@ import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
@@ -26,7 +31,12 @@ public class LanguageHelper {
     private static final String filename = "minecraft.zh_cn.json";
     private static Map<String, String> lang;
     static {
-        lang = FileUtil.readJsonFromFile(filename);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+            LanguageHelper.class.getResourceAsStream(filename), StandardCharsets.UTF_8
+        ));
+        Gson gson = new Gson();
+        Type type = new TypeToken<Map<String, String>>(){}.getType();
+        lang = gson.fromJson(reader, type);
     }
 
     /**
