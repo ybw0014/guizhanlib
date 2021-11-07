fb19c7274d2f5b61c2e01ef690109034903ae8a2
ybw0014 <i@guizhanss.com>
2021-11-07 15:49:23 +0800

    频率限制

---
+++ src/main/java/net/guizhanss/minecraft/guizhanlib/common/RateLimit.java
@@ -0,0 +1,117 @@
+package net.guizhanss.minecraft.guizhanlib.common;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * 频率限制
+ *
+ * @param <K> 频率键的类型
+ * @author ybw0014
+ */
+public class RateLimit<K> {
+    private final long rateTime;
+    private final long rateVisits;
+    private Map<K, Long> timeMap; // 重置时间
+    private Map<K, Long> visitMap; // 访问次数
+
+    /**
+     * 构建函数
+     * @param time 时间(毫秒)
+     * @param visits 访问次数
+     */
+    public RateLimit(long time, long visits) {
+        this.rateTime = time;
+        this.rateVisits = visits;
+        this.timeMap = new HashMap<>();
+        this.visitMap = new HashMap<>();
+    }
+
+    /**
+     * 获取总限制次数
+     * @return 总限制次数
+     */
+    public long getLimit() {
+        return this.rateVisits;
+    }
+
+    /**
+     * 获取已用次数
+     * @param key 查询键
+     * @return 已用次数
+     */
+    public long getUsed(K key) {
+        Optional<Long> time = Optional.ofNullable(this.timeMap.get(key));
+        if (time.isPresent()) {
+            if (time.get() > System.currentTimeMillis()) {
+                return 0;
+            }
+
+            Optional<Long> visits = Optional.ofNullable(this.visitMap.get(key));
+
+            if (visits.isPresent()) {
+                return visits.get();
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 获取剩余次数
+     * @param key 查询键
+     * @return 剩余次数
+     */
+    public long getRemaining(K key) {
+        return this.rateVisits - this.getUsed(key);
+    }
+
+    /**
+     * 增加访问次数
+     * @param key 查询键
+     * @param visits 访问次数
+     * @return 是否成功增加访问次数
+     */
+    public boolean add(K key, long visits) {
+        if (this.getRemaining(key) < visits) {
+            return false;
+        }
+
+        Optional<Long> time = Optional.ofNullable(this.timeMap.get(key));
+        if (time.isPresent()) {
+            if (time.get() > System.currentTimeMillis()) {
+                this.reset(key);
+                this.timeMap.put(key, System.currentTimeMillis() + this.rateTime);
+            }
+        } else {
+            this.timeMap.put(key, System.currentTimeMillis() + this.rateTime);
+        }
+
+        Optional<Long> pVisits = Optional.ofNullable(this.visitMap.get(key));
+        if (pVisits.isPresent()) {
+            this.visitMap.put(key, visits + pVisits.get());
+        } else {
+            this.visitMap.put(key, visits);
+        }
+
+        return true;
+    }
+
+    /**
+     * 增加1次访问次数
+     * @param key 查询键
+     * @return 是否成功增加访问次数
+     */
+    public boolean add(K key) {
+        return this.add(key, 1);
+    }
+
+    /**
+     * 重置频率限制
+     * @param key 查询键
+     */
+    public void reset(K key) {
+        this.timeMap.remove(key);
+        this.visitMap.remove(key);
+    }
+}
