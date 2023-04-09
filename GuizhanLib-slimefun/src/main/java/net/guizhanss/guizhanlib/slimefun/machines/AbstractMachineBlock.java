058393c1e3d1a454f9838b1617679dd1bca2af4e
ybw0014 <i@guizhanss.com>
2023-04-09 03:57:01 -0400

    Slimefun machine

---
+++ GuizhanLib-slimefun/src/main/java/net/guizhanss/guizhanlib/slimefun/machines/AbstractMachineBlock.java
@@ -0,0 +1,67 @@
+package net.guizhanss.guizhanlib.slimefun.machines;
+
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
+import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType;
+import lombok.Setter;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@Setter
+@ParametersAreNonnullByDefault
+public abstract class AbstractMachineBlock extends TickingMenuBlock implements EnergyNetComponent {
+
+    protected int energyPerTick = -1;
+    protected int energyCapacity = -1;
+
+    public AbstractMachineBlock(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(itemGroup, item, recipeType, recipe);
+    }
+
+    @Override
+    protected void tick(Block b, BlockMenu menu) {
+        if (getCharge(menu.getLocation()) < energyPerTick) {
+            if (menu.hasViewer()) {
+                menu.replaceExistingItem(getStatusSlot(), getNoEnergyItem());
+            }
+        } else if (process(b, menu)) {
+            removeCharge(menu.getLocation(), energyPerTick);
+        }
+    }
+
+    protected abstract boolean process(Block b, BlockMenu menu);
+
+    protected abstract int getStatusSlot();
+
+    protected abstract ItemStack getNoEnergyItem();
+
+    @Override
+    public final int getCapacity() {
+        return energyCapacity;
+    }
+
+    @Nonnull
+    @Override
+    public final EnergyNetComponentType getEnergyComponentType() {
+        return EnergyNetComponentType.CONSUMER;
+    }
+
+    @Override
+    public final void register(@Nonnull SlimefunAddon addon) {
+        if (energyPerTick == -1) {
+            throw new IllegalStateException("You must call .energyPerTick() before registering!");
+        }
+        if (energyCapacity == -1) {
+            energyCapacity = energyPerTick * 2;
+        }
+        super.register(addon);
+    }
+
+}
---
+++ GuizhanLib-slimefun/src/main/java/net/guizhanss/guizhanlib/slimefun/machines/TickingMenuBlock.java
@@ -0,0 +1,52 @@
+package net.guizhanss.guizhanlib.slimefun.machines;
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
+import me.mrCookieSlime.Slimefun.Objects.handlers.BlockTicker;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+/**
+ * A {@link TickingMenuBlock} is a {@link MenuBlock} with {@link BlockTicker}.
+ * <p>
+ * Modified from InfinityLib
+ *
+ * @author Mooy1
+ * @author ybw0014
+ */
+@ParametersAreNonnullByDefault
+public abstract class TickingMenuBlock extends MenuBlock {
+
+    public TickingMenuBlock(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(itemGroup, item, recipeType, recipe);
+
+        addItemHandler(new BlockTicker() {
+            @Override
+            public boolean isSynchronized() {
+                return isSync();
+            }
+
+            @Override
+            public void tick(Block b, SlimefunItem item, Config data) {
+                BlockMenu menu = BlockStorage.getInventory(b);
+                if (menu != null) {
+                    TickingMenuBlock.this.tick(b, menu);
+                }
+            }
+
+        });
+    }
+
+    protected abstract void tick(Block b, BlockMenu menu);
+
+    protected boolean isSync() {
+        return false;
+    }
+}
