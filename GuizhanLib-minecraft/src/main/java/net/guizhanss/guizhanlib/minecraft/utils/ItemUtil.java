40d5ddc35e2cfdaae5f5c605041e82b965aab3b1
ybw0014 <i@guizhanss.com>
2023-10-12 23:43:50 -0400

    feat: add `ItemUtil`

---
+++ GuizhanLib-minecraft/src/main/java/net/guizhanss/guizhanlib/minecraft/utils/ItemUtil.java
@@ -0,0 +1,32 @@
+package net.guizhanss.guizhanlib.minecraft.utils;
+
+import lombok.experimental.UtilityClass;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@UtilityClass
+@SuppressWarnings("deprecation")
+public class ItemUtil {
+    /**
+     * Append lore to an {@link ItemStack}.
+     *
+     * @param itemStack
+     *     The {@link ItemStack} to append lore.
+     * @param extraLore
+     *     The lore to append.
+     *
+     * @return The {@link ItemStack} with lore appended.
+     */
+    public static ItemStack appendLore(ItemStack itemStack, String... extraLore) {
+        ItemMeta meta = itemStack.getItemMeta();
+        List<String> lore = meta.hasLore() ? meta.getLore() : new ArrayList<>();
+        lore.addAll(ChatUtil.color(Arrays.asList(extraLore)));
+        meta.setLore(lore);
+        itemStack.setItemMeta(meta);
+        return itemStack;
+    }
+}
