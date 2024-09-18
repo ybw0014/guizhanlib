4ce521087cc1665bcf42aef54708eba07572941b
ybw0014 <i@guizhanss.com>
2024-09-18 14:52:33 -0700

    chore: remove unnecessary

--- guizhanlib-minecraft/build.gradle.kts
+++ guizhanlib-minecraft/build.gradle.kts
@@ -1,12 +1,6 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 dependencies {
     api(project(":guizhanlib-common"))
     api(project(":guizhanlib-localization"))
     api("io.papermc:paperlib:1.0.8")
     testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.93.2")
-}
-
-tasks.withType<ShadowJar> {
-    relocate("io.papermc.lib", "net.guizhanss.guizhanlib.libs.paperlib")
 }
