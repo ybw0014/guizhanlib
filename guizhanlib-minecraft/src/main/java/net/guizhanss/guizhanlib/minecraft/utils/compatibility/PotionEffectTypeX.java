47263fd441abc578b13e04b740b9d7548a3ee64f
ybw0014 <i@guizhanss.com>
2024-10-04 09:58:39 -0700

    fix: fix PotionEffectTypeX

--- guizhanlib-minecraft/src/main/java/net/guizhanss/guizhanlib/minecraft/utils/compatibility/PotionEffectTypeX.java
+++ guizhanlib-minecraft/src/main/java/net/guizhanss/guizhanlib/minecraft/utils/compatibility/PotionEffectTypeX.java
@@ -1,12 +1,14 @@
 package net.guizhanss.guizhanlib.minecraft.utils.compatibility;
 
 import lombok.experimental.UtilityClass;
+import net.guizhanss.guizhanlib.minecraft.utils.MinecraftVersionUtil;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Registry;
 import org.bukkit.potion.PotionEffectType;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.lang.reflect.Field;
 
 /**
  * This class holds {@link PotionEffectType} that are renamed in 1.20.5.
@@ -25,19 +27,32 @@ public class PotionEffectTypeX {
     public static final PotionEffectType RESISTANCE;
 
     static {
-        SLOWNESS = getByKey("slowness");
-        HASTE = getByKey("haste");
-        MINING_FATIGUE = getByKey("mining_fatigue");
-        STRENGTH = getByKey("strength");
-        INSTANT_HEALTH = getByKey("instant_health");
-        INSTANT_DAMAGE = getByKey("instant_damage");
-        JUMP_BOOST = getByKey("jump_boost");
-        NAUSEA = getByKey("nausea");
-        RESISTANCE = getByKey("resistance");
+        // EFFECT registry was added in 1.20.3, use legacy field access before that
+        boolean isAtLeast1_20_3 = MinecraftVersionUtil.isAtLeast(20, 3);
+
+        SLOWNESS = isAtLeast1_20_3 ? getByKey("slowness") : getField("SLOW");
+        HASTE = isAtLeast1_20_3 ? getByKey("haste") : getField("FAST_DIGGING");
+        MINING_FATIGUE = isAtLeast1_20_3 ? getByKey("mining_fatigue") : getField("SLOW_DIGGING");
+        STRENGTH = isAtLeast1_20_3 ? getByKey("strength") : getField("INCREASE_DAMAGE");
+        INSTANT_HEALTH = isAtLeast1_20_3 ? getByKey("instant_health") : getField("HEAL");
+        INSTANT_DAMAGE = isAtLeast1_20_3 ? getByKey("instant_damage") : getField("HARM");
+        JUMP_BOOST = isAtLeast1_20_3 ? getByKey("jump_boost") : getField("JUMP");
+        NAUSEA = isAtLeast1_20_3 ? getByKey("nausea") : getField("CONFUSION");
+        RESISTANCE = isAtLeast1_20_3 ? getByKey("resistance") : getField("DAMAGE_RESISTANCE");
     }
 
     @Nullable
     private static PotionEffectType getByKey(@Nonnull String key) {
         return Registry.EFFECT.get(NamespacedKey.minecraft(key));
+    }
+
+    @Nullable
+    private static PotionEffectType getField(@Nonnull String key) {
+        try {
+            Field field = PotionEffectType.class.getDeclaredField(key);
+            return (PotionEffectType) field.get(null);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
